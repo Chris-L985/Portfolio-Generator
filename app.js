126f758d044eb2c275d5bdf70e7b3e27ac4c33bc
Chris-L985 <thelopezdispenser@gmail.com>
2021-06-16 23:51:35 -0600

    added inquirer to app

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+console.log(inquirer);
 
-const profileDataArgs = process.argv.slice(2);
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+//const fs = require('fs');
+//const generatePage = require('./src/page-template');
 
-const [name, github] = profileDataArgs;
+
+//const pageHTML = generatePage(name, github);
 
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
+//fs.writeFile('./index.html', pageHTML, err => {
+//    if (err) throw err;
 
-    console.log('Portfolio complete!')
-});
+//    console.log('Portfolio complete!')
+//});
